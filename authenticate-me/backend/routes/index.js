c9bff08881752a595cb61110a0c7060aa2ffd82b
Taylor <digitalidentity21@gmail.com>
2021-07-14 16:12:01 -0700

    phase 2 error handling

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+router.get('/', function(req, res) {
+    res.send(`server operational`)
+})
 
 module.exports = router;
