2eb5d9c1061e35d44c92832bb33d58804f1c6aa6
Taylor <digitalidentity21@gmail.com>
2021-07-15 10:30:32 -0700

    log out route complete

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -32,7 +32,14 @@ router.post(
   );
 
 
-
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 
 
