fb8d19051644fe4e7f3dbd5880181a7d4e28584c
Taylor <digitalidentity21@gmail.com>
2021-07-15 15:52:58 -0700

    phase 1 front end complete

--- authenticate-me/frontend/src/store/index.js
+++ authenticate-me/frontend/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
+  session: sessionReducer,
 });
+
 
 let enhancer;
 
@@ -19,5 +22,8 @@ if (process.env.NODE_ENV === 'production') {
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
   };
+
+
+
   
   export default configureStore;
