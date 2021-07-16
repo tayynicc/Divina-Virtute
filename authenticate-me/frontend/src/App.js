2c144ee3830326954ec9be15fbad83dc08c20fde
Taylor <digitalidentity21@gmail.com>
2021-07-16 10:20:36 -0700

    sign up complete

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
