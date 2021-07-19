c468417cae32c4183cfb9c51962990ed4968cb49
tayynicc <digitalidentity21@gmail.com>
2021-07-18 22:55:18 -0700

    home layouts

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -14,13 +14,36 @@ function Home(){
                 <div className='theme'></div>
                 
                 <div className='header'></div>
-                   
+                    <div className='home__logo'></div>
+                    <div className='home__search'></div>
+                    <div className='home__links'></div>
+                    <div className='home__addProduct'></div>
+                    <div className='home__pfp'></div>
                 
-                <div className='previous'></div>
+                <div className='previous'>
+                    <div className='previous__mockup'>
+                        <div className='previous__image'></div>
+                        <div className='previous__name'></div>
+                        <div className='previous__tagLine'></div>
+                    </div>
+                </div>
 
-                <div className='product__display'></div>
+                <div className='product__display'>
+                    <div className='pd__mockup'>
+                        <div className='pd__image'></div>
+                        <div className='pd__name'></div>
+                        <div className='pd__tagLine'></div>
+                        <div className='pd__commentNum'></div>
+                        <div className='pd__tag'></div>
+                        <div className='pd__voteCount'></div>
+                    </div>
+                </div>
 
-                <div className='newArivals'></div>
+                <div className='newArival'>
+                    <div className='newArival__image'></div>
+                    <div className='newArival__name'></div>
+                    <div className='newArival__tagLine'></div>
+                </div>
 
                 
                 
