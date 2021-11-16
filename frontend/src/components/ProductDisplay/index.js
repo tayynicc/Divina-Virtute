cc34d89f253606884936e38a06d3489078ad614d
Taylor <digitalidentity21@gmail.com>
2021-11-16 14:38:58 -0700

    product display fixed

--- frontend/src/components/ProductDisplay/index.js
+++ frontend/src/components/ProductDisplay/index.js
@@ -24,19 +24,19 @@ function ProductDisplay(){
 
     return(
         <div>
-            <h2 className='product__display-label'>Products</h2>
+            {/* <h2 className='product__display-label'>Products</h2> */}
             <div className='product__display'>
                 
                 
                 
                 {products?.map((product) => 
-                    <div className='pd__mockup' key={`${product.title}-${product.id}`}>
-                        
+                    <div className='pd__tile-home' key={`${product.title}-${product.id}`}>
+{/*                         
                         <a href={`/products/${product.id}`} className='pd__title'>{product.title}</a>
-                        <p className='pd__tagLine'>{product.tagLine}</p>
-                        <p className='pd__price'>{`$ ${product.price}`}</p>
-                        <img className='pd__image' src={product.imageUrl} alt="BigCo Inc. logo"/>
-                        
+                        <p className='pd__tagLine'>{product.tagLine}</p>*/}
+                        <p className='pd__price-home'>{`$ ${product.price}`}</p>
+                        <a className='pd__image-home' href={`/products/${product.id}`}><img className='pd__image-home' src={product.imageUrl} alt="BigCo Inc. logo"/></a>
+                         
                         
                     </div>
                 )} 
