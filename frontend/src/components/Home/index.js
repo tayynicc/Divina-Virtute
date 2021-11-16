528f02a8fe2625a1c352403626ff88a72e0d536b
Taylor <digitalidentity21@gmail.com>
2021-11-16 11:24:08 -0700

    new arrivals section

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
+import { NavLink } from 'react-router-dom'
 import { updatedProducts } from '../../store/products'
 import { getOneUser } from '../../store/session'
 import './Home.css'
@@ -33,28 +34,35 @@ function Home(){
         dispatch(updatedProducts())
     }, [dispatch, userId])
 
+
+    const condensedTitle = (title) => {
+       let condensed = title.split(',')
+       return condensed 
+    }
   
     return (
         <div>
             <div className='bkg__login-layer1'>
                 <div className='theme'></div>
                 
-                <h2 className="new__products-label hidden">New Arrivals</h2>    
+                {/* <h2 className="new__products-label hidden">New Arrivals</h2>     */}
                     
                     <div className='previous'>
                     
                         {products.newest && products.newest.map((product) => (
                         <div className='previous__mockup' key={product.id}>
-                            <a herf={`/products/${product.id}`}><div><img className='previous__image'src={product.imageUrl}></img></div></a>
-                            <a href={`/products/${product.id}`}><div className='previous__name'>{product.title}</div></a>
+                            <a href={`/products/${product.id}`}><div><img className='previous__image'src={product.imageUrl}></img></div></a>
+                            <a href={`/products/${product.id}`}><div className='previous__name'>{condensedTitle(product.title)}</div></a>
                             <div className='previous__tagLine'>{product.tagLine}</div>
+                            <a href={`/products/${product.id}`}><div className='learnMore'>... Learn More </div></a>
                         </div>  
                         ))}
-                        
                     </div>
 
+                    <div className='divider' ></div>
+
     
-                <h2 className="users__products-label ">My Products</h2>   
+                {/* <h2 className="users__products-label ">My Products</h2>   
                 <div className='newArival'>
                     {currUsersProducts?.map((product) => (
                       <div className='newArrivals__mockup'>
@@ -63,7 +71,7 @@ function Home(){
                         <div className='newArival__tagLine'>{product.tagLine}</div>
                       </div>   
                     ))}
-                </div>
+                </div> */}
 
                 
                 
