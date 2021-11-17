3e22894bf0e76a415fd7544c81e01b1a657b4129
Taylor <digitalidentity21@gmail.com>
2021-11-17 14:48:19 -0700

    product discussion restyled

--- frontend/src/components/ProductDiscussion/index.js
+++ frontend/src/components/ProductDiscussion/index.js
@@ -104,7 +104,7 @@ function ProductDisplay(){
             </div>
             <div className='product-discussion__outerDiv'>
                 <form onSubmit={handleSubmit} className='commentInput__container'>
-                    {errors.length && (<p className='error_message-review'>{errors}</p>)}
+                    {errors && (<p className='error_message-review'>{errors}</p>)}
                     <textarea  id='textbox' value={review} onChange={updateReview}className='input__box'></textarea>
                     <button type='submit' className='addComment__button'>Add Comment</button>
 
@@ -114,24 +114,15 @@ function ProductDisplay(){
                     {reviews?.map((review) => 
                         <div className='comment' key={review.id}>
                            
-                           <a href={`/profile/${review.User.id}`}><img alt='users profile' className='pfp__image-comment' src={review.User.imageUrl}></img></a>
+                            <a href={`/profile/${review.User.id}`}><img alt='users profile' className='pfp__image-comment' src={review.User.imageUrl}></img></a>
                            
-
-                            {/* <a href={`/profile/${review.User.id}`}></a>*/}
                             <h4 className='pfp__username'>{review.User.username}</h4>
-                            {/* {review.updatedAt.toDateString()} */}
                             <p className='posted'>{formatDate(review.updatedAt)}</p>
                            
-
                             <p className='comment__body'>{review.review}</p>
                             
-        
-                            
-                        
                             <button className='delete__review' onClick={() => handleDelete(+review.id)}><img alt='delete button' className='edit__comment-delete' src="https://img.icons8.com/material-outlined/24/000000/trash--v1.png"/></button>
                             
-                            {/* <img className='edit__comment-edit' src="https://img.icons8.com/ios-glyphs/30/000000/edit--v2.png"/> */}
-                    
                         
                         </div>
                     ).reverse()} 
